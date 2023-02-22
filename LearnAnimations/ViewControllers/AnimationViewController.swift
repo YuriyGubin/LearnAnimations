09a745681e4e6ff11a903fde505c997871fef7f0
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-22 13:27:24 +0500

    Setup animation values

--- LearnAnimations/ViewControllers/AnimationViewController.swift
+++ LearnAnimations/ViewControllers/AnimationViewController.swift
@@ -21,11 +21,13 @@ class AnimationViewController: UIViewController {
         animationVeiw.layer.cornerRadius = 10
     }
 
+   
     @IBAction func runAnimationButton(_ sender: UIButton) {
         let animationRandomIndex = Int.random(in: 0..<animations.count)
         let curveRandomIndex = Int.random(in: 0..<animations.count)
         let force = Double.random(in: 1...1.5)
-        let duration = Double.random(in: 2...4)
+        let duration = Double.random(in: 0.5...1.2)
+        let delay = 0.3
         
         let animation = animations[animationRandomIndex].animationName
         let curve = animations[curveRandomIndex].curveName
@@ -35,10 +37,16 @@ class AnimationViewController: UIViewController {
         animationVeiw.curve = curve
         animationVeiw.force = force
         animationVeiw.duration = duration
-        animationVeiw.delay = 0.3
+        animationVeiw.delay = delay
         animationVeiw.animate()
         
-        
+        animationInfoLabel.text = """
+preset: \(animation)
+curve: \(curve)
+force: \(String(format: "%.2f", force))
+duration: \(String(format: "%.2f", duration))
+delay: \(String(format: "%.2f", delay))
+"""
     }
     
     
